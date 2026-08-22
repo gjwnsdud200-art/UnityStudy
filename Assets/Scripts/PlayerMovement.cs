eb40c673e23e47211241799867062df62f72f3ea
허준영 <gjwnsdud200@gmail.com>
2026-08-22 14:35:15 +0900

    Implement player movement and ground check

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public float jumpForce = 8f;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private bool isGround;
+
     void Start()
     {
         Debug.Log("게임 시작!");
@@ -9,9 +18,22 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        isGround = Physics2D.OverlapCircle(
+            groundCheck.position,
+            0.2f,
+            groundLayer
+            );
+        float move = Input.GetAxisRaw("Horizontal");
+
+        //transform.position += Vector3.right * move * speed * Time.deltaTime;
+
+        //rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+
+        rb.AddForce(Vector2.right * move * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGround) 
         {
-            Debug.Log("스페이스바가 눌렀습니다!");
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }

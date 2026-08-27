0c081e81c628323bb26e5cd7152ebce8169c1059
허준영 <gjwnsdud200@gmail.com>
2026-08-27 15:16:58 +0900

    feat : add slide animation and refactoring player code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,72 @@ public class PlayerMovement : MonoBehaviour
 {
     private SpriteRenderer sr;
     private Animator animator;
-    public float speed = 0.1f;
     private Rigidbody2D rb;
     
     public float jumpForce = 8f;
+    public float speed = 0.1f;
 
     public Transform groundCheck;
     public LayerMask groundLayer;
 
     private bool isGround;
+    private bool isSlide;
 
+    void Move(float move)
+    {
+        rb.AddForce(Vector2.right * move * speed);
+    }
+
+    void Flip(float move)
+    {
+        if (move > 0)
+        {
+            sr.flipX = false;
+        }
+        else if (move < 0)
+        {
+            sr.flipX = true;
+        }
+    }
+
+   bool SlideInput()
+    {
+        return Input.GetKey(KeyCode.LeftControl) && isGround;
+        
+    }
+    void UpdateAnimator(float move)
+    {
+        animator.SetBool("isGround", isGround);
+        animator.SetBool("isRunning", move != 0);
+        animator.SetBool("isSlide", isSlide);
+    }
+
+    bool CheckGround()
+    {
+        return Physics2D.OverlapCircle(
+            groundCheck.position,
+            0.2f,
+            groundLayer
+            );
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && isGround)
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    /* -----------------------------------------------------------------------------------*/
     void Start()
     {
         Debug.Log("게임 시작!");
@@ -25,39 +81,21 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        isGround = Physics2D.OverlapCircle(
-            groundCheck.position,
-            0.2f,
-            groundLayer
-            );
-        animator.SetBool("isGround", isGround);
 
+
+        isGround = CheckGround();
+        isSlide = SlideInput();
+        Debug.Log(isGround);
         float move = Input.GetAxisRaw("Horizontal");
-        if (move > 0)
-        {
-            sr.flipX = false;
-        }
-        else if (move < 0)
-        {
-            sr.flipX = true;
-        }
-
-        animator.SetBool("isRunning", move != 0);
+        Move(move);
+        Flip(move);
+        UpdateAnimator(move);
+       
 
         //transform.position += Vector3.right * move * speed * Time.deltaTime;
 
         //rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
-
-        rb.AddForce(Vector2.right * move * speed);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGround) 
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
-
-        if (Input.GetKeyDown(KeyCode.J) && isGround)
-        {
-            animator.SetTrigger("Attack");
-        }
+        Jump();
+        Attack();
     }
 }

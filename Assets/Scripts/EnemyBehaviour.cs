0f2660969698916c5f14365fa6a321236e6fe071
허준영 <gjwnsdud200@gmail.com>
2026-09-11 15:58:57 +0900

    featt : add enemy patrol, chase, contact damege

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    public float moveSpeed = 2f;
+    public float patrolDistance = 3f;
+
+    private float startX; // 적이 처음 시작한 X위치기억용.
+    private int direction = 1;
+
+    public Transform player; //  플레이어 실제 위치를 알기위한 참조
+    public float chaseDistance = 5f;
+    public float maxChaseDistance = 8f;
+
+    private bool isChasing = false;
+ 
+
+    public int damage = 20; // 충돌시 데미지
+
+    private void OnCollisionEnter2D(Collision2D collision) // 부딫히면 데미지받게
+    {
+        PlayerHealth playerHealth =
+            collision.gameObject.GetComponent<PlayerHealth>();
+
+        if (playerHealth != null)
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }
+
+    /*
+     Unity Physics
+     ↓
+"두 Collider2D가 충돌했다!"
+     ↓
+해당 MonoBehaviour에
+OnCollisionEnter2D가 있는지 확인
+     ↓
+있으면 Unity가 호출
+     */
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        startX = transform.position.x;
+
+    }
+
+    void Patrol()
+    {
+        
+        transform.position +=
+            Vector3.right * direction * moveSpeed * Time.deltaTime;
+
+        if (transform.position.x >= startX + patrolDistance)
+        {
+            direction = -1; // 방향 반대로.
+            sr.flipX = true; // 스프라이트 좌우반전
+        }
+        else if (transform.position.x <= startX - patrolDistance)
+        {
+            direction = 1;
+            sr.flipX = false;
+        }
+    }
+
+    void ChasePlayer()
+    {
+        if (player.position.x > transform.position.x)
+        {
+            direction = 1;
+            sr.flipX = false;
+        }
+        else 
+        {
+            direction = -1;
+            sr.flipX = true;
+        }
+
+        transform.position +=
+            Vector3.right * direction * moveSpeed * Time.deltaTime;
+
+ 
+    }
+
+    
+    void Update()
+    {
+        float distanceToPlayer =
+            Vector2.Distance(transform.position, player.position);
+
+        float distanceFromStart =   // 계속해서 Chase하는걸 방지하기위해 일정거리 이상하면 다시 Patrol하게
+            Mathf.Abs(transform.position.x - startX); // Mathf.Abs를 통해 오른쪽이건 왼쪽이건 음수라도 거리만 알게.
+
+        if (!isChasing)
+        {
+            if (distanceToPlayer <= chaseDistance)
+            {
+                isChasing = true;
+            }
+        }
+
+        if (isChasing)
+        {
+            if (distanceFromStart >= maxChaseDistance)
+            {
+                isChasing = false;
+            }
+        }
+
+        if (isChasing)
+        {
+            ChasePlayer();
+        }
+        else 
+        {
+            Patrol();
+        }
+    }
+}
